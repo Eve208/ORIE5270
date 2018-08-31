9e42da286fab31b51e52f10e00730fa91bde0e3c
Yiwen Jiang <32209449+Eve208@users.noreply.github.com>
2018-08-31 16:30:08 -0400

    Update pysaprk_kmeans.py

--- hw4/kmeans/pysaprk_kmeans.py
+++ hw4/kmeans/pysaprk_kmeans.py
@@ -14,16 +14,18 @@ def pyspark_kmeans(data_file, c_file, max_iter=100):
     c = np.array(c_ini[:])
     
     for num_iter in range(max_iter):
+        # group the points by centroids
         group = data.map(lambda l: (np.argmin([np.linalg.norm(l-i) for i in c]), (l, 1)))
         group_sum = group.reduceByKey(lambda n1, n2: (n1[0] + n2[0], n1[1] + n2[1])).sortByKey(ascending=True)
-
+        # new centroids
         c_new = np.array(group_sum.map(lambda l: l[1][0]/l[1][1]).collect())
         
         if np.linalg.norm(c - c_new) == 0:
             break
         else:
             c = c_new
-
+    
+    # write the final seeds to text file
     file = open("final.txt", "w")
     for items in c:
         temp = ""
