ce4755584e0350241b10c4c0d4c1a276c117e988
Yiwen Jiang <32209449+Eve208@users.noreply.github.com>
2018-08-30 23:44:25 -0400

    adding Matrix.py

---
+++ hw4/matrix/Matrix.py
@@ -0,0 +1,33 @@
+from pyspark import SparkConf, SparkContext
+import numpy as np
+
+def mat_mult_vec(matrix_file,vector_file):
+    """
+    :param matrix_file:(text file) contains the matrix, with the first col as row index
+    :param vector_file:(text file) contains the vector
+    :return: (rdd object) contains the output vector
+    """
+    mat = sc.textFile(matrix_file).map(lambda l: np.array([float(i) for i in l.split(",")])).cache()
+    vec = sc.textFile(vector_file).map(lambda l: np.array([float(i) for i in l.split(",")])).cache()
+    
+    mat = mat.map(lambda l: (l[0], ((l[1+i], i) for i in range(len(l[1:])))))
+    mat_flat = mat.flatMap(matCol)
+    vec = vec.flatMap(addVecInd)
+    
+    mat_join = mat_flat.join(vec).map(lambda l: (l[1][0][0], l[1][0][1]*l[1][1]))
+    result = mat_join.reduceByKey(lambda n1, n2: n1 + n2).map(lambda l: l[1])
+    return result
+
+def matCol(l):
+    for i in l[1]:
+        yield(i[1], (l[0], i[0]))
+
+def addVecInd(l):
+    for i in range(len(l)):
+        yield(i, l[i])
+
+
+if __name__ == '__main__':
+    sc = SparkContext('local[4]', 'matrix_mult')
+    mat_mult_vec("A.txt", "B.txt")
+    sc.stop()
